e331b9edd01d823053a599f428abc870c9ed10b2
Sharif <sharifbdp@gmail.com>
2016-12-28 11:55:25 +0600

    modify base url

--- application/config/config.php
+++ application/config/config.php
@@ -22,7 +22,7 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'YOUR_PROJECT_BASE_URL';
 
 /*
 |--------------------------------------------------------------------------
